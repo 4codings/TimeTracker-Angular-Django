20520ebe30942ec457eb8663b45d78ef1d304e29
4codings <proudofdeveloper@gmail.com>
2019-04-23 10:29:06 -0400

    adding test

--- spec/features/user_visits_show_page_spec.rb
+++ spec/features/user_visits_show_page_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 require 'launchy'
 
 feature 'user visits show page'  do
-  scenario 'user sees stops' do
-
-    visit stops_path
+  scenario 'user sees the correct stop' do
 
     first_stop = Stop.create(name: "eric")
+    second_stop = Stop.create(name: "Shard")
 
-    expect(page).to_not have_content(first_stop)
+    visit stop_path(first_stop.id)
+
+
+    expect(page).to have_content(first_stop.name)
+    expect(page).to_not have_content(second_stop.name)
   end
 
 end
