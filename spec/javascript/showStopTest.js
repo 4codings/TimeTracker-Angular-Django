25f15722848f51578ffc8f5a2baeffdf103777c1
4codings <proudofdeveloper@gmail.com>
2019-04-24 15:18:54 -0400

    The final uneffening. DONE!!!

--- spec/javascript/showStopTest.js
+++ spec/javascript/showStopTest.js
@@ -26,17 +26,8 @@ describe('show page test', () => {
       expect(component.find('.id')).toBePresent()
       expect(component.find('.name')).toBePresent()
       expect(component.state('stop')).toEqual({ id:1, name:"El Greco",created_at:"2019-04-22T18:53:17.323Z",updated_at:"2019-04-22T18:53:17.323Z" });
-      
       expect(component.find('.name').text()).toEqual("Name: El Greco")
       done()
     },0)
   });
 });
-//
-// it('renders a list item for each item returned from the api call', (done) => {
-//    setTimeout(() => {
-//      expect(wrapper.find('li').length).toEqual(tasks.length)
-//      expect(wrapper.find('li').text()).toEqual(tasks[0].title)
-//      done()
-//    }, 0)
-//  })
