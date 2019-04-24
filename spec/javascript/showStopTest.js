06e2f002709d0162df1b987a6d10cb23d02a8bb2
4codings <proudofdeveloper@gmail.com>
2019-04-24 15:15:28 -0400

    The uneffening, part I?

--- spec/javascript/showStopTest.js
+++ spec/javascript/showStopTest.js
@@ -21,12 +21,13 @@ describe('show page test', () => {
 
   afterEach(() => fetchMock.restore)
 
-  it('should show labels, fetch data from api and present', () => {
+  it('should show labels, fetch data from api and present', (done) => {
     setTimeout(() => {
       expect(component.find('.id')).toBePresent()
       expect(component.find('.name')).toBePresent()
       expect(component.state('stop')).toEqual({ id:1, name:"El Greco",created_at:"2019-04-22T18:53:17.323Z",updated_at:"2019-04-22T18:53:17.323Z" });
-      expect(component.find('.name').text()).to.equal('El Greco')
+      
+      expect(component.find('.name').text()).toEqual("Name: El Greco")
       done()
     },0)
   });
