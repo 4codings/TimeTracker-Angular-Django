2c9a98b2b2023bde7496d4477710bc8eea8cc3c1
4codings <proudofdeveloper@gmail.com>
2019-04-24 14:46:16 -0400

    adding done to enzyme test

--- spec/javascript/showStopTest.js
+++ spec/javascript/showStopTest.js
@@ -21,12 +21,21 @@ describe('show page test', () => {
 
   afterEach(() => fetchMock.restore)
 
-  it('should show labels, fetch data from api and present', () => {
+  it('should show labels, fetch data from api and present', (done) => {
     setTimeout(() => {
       expect(component.find('.id')).toBePresent()
       expect(component.find('.name')).toBePresent()
       expect(component.state('stop')).toEqual({ id:1, name:"El Greco",created_at:"2019-04-22T18:53:17.323Z",updated_at:"2019-04-22T18:53:17.323Z" });
       expect(component.find('.name').text()).to.equal('El Greco')
+      done()
     },0)
   });
 });
+//
+// it('renders a list item for each item returned from the api call', (done) => {
+//    setTimeout(() => {
+//      expect(wrapper.find('li').length).toEqual(tasks.length)
+//      expect(wrapper.find('li').text()).toEqual(tasks[0].title)
+//      done()
+//    }, 0)
+//  })
