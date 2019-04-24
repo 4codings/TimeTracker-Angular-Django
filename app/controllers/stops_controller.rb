f6d96c414e5e6e0360fdf25941c2e361d05d959a
4codings <proudofdeveloper@gmail.com>
2019-04-24 15:52:57 -0400

    breaking out hash to multiple lines, removing stray empty comment

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -7,4 +7,20 @@ class StopsController < ApplicationController
     @stop=Stop.find(params[:id])
   end
 
+  def create
+    new_stop = Stop.new(params[:stop])
+
+    if new_stop.save
+      flash[:notice] = "stop created successfully"
+
+    else
+      flash[:error] = "error"
+
+    end
+  end
+
+  def new
+
+  end
+
 end
